b2e6bdfcd4dca4bc9af7605868f93bdf1f8dfa41
nicoasesino <soporte@cybercenter.cl>
2023-02-06 18:22:12 -0300

    modificacion

---
+++ script.js
@@ -0,0 +1,20 @@
+let juegos = [{id: 1, nombre: "Minecraft", precio: 19.000, categoria: "sandbox", imgURL: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/1a/MCJE_key_art.jpg/revision/latest/smart/width/250/height/250?cb=20210910122803"},
+            {id: 2, nombre: "GTA V", precio: 19.000, categoria: "survival"},
+            {id: 3, nombre: "Counter Strike Global Ofensive", precio: 19.000, categoria: "shooter"}
+]
+
+
+
+for (const juego of juegos) {
+    
+}
+
+
+
+
+
+
+
+
+// let buscadorDeJuegos = juegos.find(juego => juego.id === 2)
+// console.log(buscadorDeJuegos)
